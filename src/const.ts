0e74f6a7bcc390734ec4ccd6dfc4ed6f2622428f
Raymond Li <it1011@qq.com>
2019-06-18 09:57:42 +0800

    fix(typing): 修复方法参数未指名类型的问题

--- src/const.ts
+++ src/const.ts
@@ -37,7 +37,7 @@ export const getThumbnailViewportSize = (realSize:ISize, viewportSize:ISize) => 
   return [boxHeight, boxWidth];
 };
 
-export const getThumbnailSize = (nHeight, nWidth) => {
+export const getThumbnailSize = (nHeight:number, nWidth:number) => {
   let delta = nWidth - nHeight;
   let height = thumbnailMaxSize;
   let width = Math.ceil((thumbnailMaxSize * nWidth) / nHeight);
